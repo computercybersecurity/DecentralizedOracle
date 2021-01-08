80b3c84996147fb78e9faaae59720a31c983f11c
peter <peter.park13ak@gmail.com>
2021-01-08 05:09:43 -0500

    Add env variables.

--- on-chain-oracle/truffle.js
+++ on-chain-oracle/truffle.js
@@ -8,6 +8,17 @@ module.exports = {
       network_id: '*',
       host: 'localhost',
       port: process.env.PORT
+    },
+    kovan: {
+      provider: function() {
+        return new HDWalletProvider(
+          //private keys array
+          process.env.MNEMONIC,
+          //url to ethereum node
+          process.env.WEB3_PROVIDER_ADDRESS
+        )
+      },
+      network_id: 42
     }
   }
 };
