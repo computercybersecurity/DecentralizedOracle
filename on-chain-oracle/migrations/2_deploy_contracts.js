a5f296e9df32d4584d76babe175904f0024f77a1
peter <peter.park13ak@gmail.com>
2021-03-12 14:43:03 -0500

    set gas limit

--- on-chain-oracle/migrations/2_deploy_contracts.js
+++ on-chain-oracle/migrations/2_deploy_contracts.js
@@ -16,24 +16,34 @@ module.exports = async function (deployer, network) {
   try {
 		let dataParse = {};
 
-    await deployer.deploy(Ownable);
+    await deployer.deploy(Ownable, {
+      gas: 1000000
+    });
     await deployer.link(Ownable, [Oracle, DEOR]);
     dataParse['Ownable'] = Ownable.address;  
 
-    await deployer.deploy(Randomizer);
+    await deployer.deploy(Randomizer, {
+      gas: 3000000
+    });
     await deployer.link(Randomizer, Selection);
     dataParse['Randomizer'] = Randomizer.address;  
   
-    await deployer.deploy(Selection);
+    await deployer.deploy(Selection, {
+      gas: 3000000
+    });
     await deployer.link(Selection, Oracle);
     dataParse['Selection'] = Selection.address;  
 
-    await deployer.deploy(SafeMathDEOR);
+    await deployer.deploy(SafeMathDEOR, {
+      gas: 1000000
+    });
     await deployer.link(SafeMathDEOR, [DEOR, Oracle]);
     dataParse['SafeMathDEOR'] = SafeMathDEOR.address;  
 
     if (!configs.DEOR) {
-      await deployer.deploy(DEOR);
+      await deployer.deploy(DEOR, {
+        gas: 5000000
+      });
       dataParse['DEOR'] = DEOR.address;  
     }
     else {
@@ -41,24 +51,34 @@ module.exports = async function (deployer, network) {
     }
   
     if (!configs.Oracles) {
-      await deployer.deploy(Oracles);
+      await deployer.deploy(Oracles, {
+        gas: 5000000
+      });
       dataParse['Oracles'] = Oracles.address;  
     }
     else {
       dataParse['Oracles'] = configs.Oracles;
     }
 
-    await deployer.deploy(Oracle, dataParse['DEOR'], dataParse['Oracles']);  
+    await deployer.deploy(Oracle, dataParse['DEOR'], dataParse['Oracles'], {
+      gas: 5000000
+    });  
     dataParse['Oracle'] = Oracle.address;
 
-    await deployer.deploy(Upgradable, dataParse['Oracle']);
+    await deployer.deploy(Upgradable, dataParse['Oracle'], {
+      gas: 1000000
+    });
     dataParse['Upgradable'] = Upgradable.address;
 
-    await deployer.deploy(PriceFeed, dataParse['Upgradable'], "ETHUSDT");
-    dataParse['ETHUSDT'] = PriceFeed.address;
+    // await deployer.deploy(PriceFeed, dataParse['Upgradable'], "ETHUSDT", {
+    //   gas: 1000000
+    // });
+    // dataParse['ETHUSDT'] = PriceFeed.address;
 
-    await deployer.deploy(DataQuery, dataParse['Upgradable'], "ETHUSDT");
-    dataParse['DataQuery'] = DataQuery.address;
+    // await deployer.deploy(DataQuery, dataParse['Upgradable'], "ETHUSDT", {
+    //   gas: 1000000
+    // });
+    // dataParse['DataQuery'] = DataQuery.address;
 
     const updatedData = JSON.stringify(dataParse);
 		await fs.promises.writeFile('contracts.json', updatedData);
